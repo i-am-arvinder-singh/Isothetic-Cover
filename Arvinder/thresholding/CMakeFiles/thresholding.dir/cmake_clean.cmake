8fbb27534cf151964793af8474bd31829c9f1551
i-am-arvinder-singh <avyarvinder@gmail.com>
2021-11-04 03:35:20 +0530

    grid divisioning coded (incomplete/imperfected)2

---
+++ Arvinder/thresholding/CMakeFiles/thresholding.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thresholding.dir/thresholding.o"
+  "CMakeFiles/thresholding.dir/thresholding.o.d"
+  "thresholding"
+  "thresholding.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thresholding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Arvinder/thresholding/CMakeFiles/thresholding.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for thresholding.
+# This may be replaced when dependencies are built.
